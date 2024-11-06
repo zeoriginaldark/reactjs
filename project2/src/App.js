0160b5aa3872e7416ce7d5b03945033e06d75b04
Darshan Subedi <darshan.subedi123@gmail.com>
2024-11-06 11:48:43 +0545

    fourth commit; frontend 1/2 done

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [foodItems, setFoodItems] = useState([]);
+  const [foodInput, setFoodInput] = useState("");
+
+  useEffect(()=>{
+    axios
+      .get('')
+  })
+
+  const addFood =() =>{
+    if (foodInput) {
+      axios
+        .post("http://localhost:5000/api/foodItems", {name: foodInput})
+        .then((response) => {
+          setFoodItems(response.data.foodItems);
+          setFoodInput("");
+        })
+        .catch((error) => console.error("Error adding food", error));
+    }
+  }
+
+  const deleteFood =(food) =>{
+    axios
+      .delete(`http://localhost:5000/api/foodItems/${food}`)
+      .then((response) => {
+        setFoodItems(response.data.foodItems);
+      })
+      .catch((error) => console.error("Error deleting food", error));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Food Manager</h1>
+      <div>
+        <input
+          type='text'
+          value={foodInput}
+          onChange={(e) => setFoodInput(e.target.value)}
+          placeholder='Add a new food item'
+        />
+        <button onClick={addFood}>Add Food</button>
+      </div>
+      <ul>
+        {foodItems.map((food, index) =>(
+          <li key={index}>
+            {food} <button onClick={()=>deleteFood(food)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
